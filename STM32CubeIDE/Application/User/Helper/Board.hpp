fa0f5705e50cf8d001aa467d6d5b8b5d09d4d33c
DuyTien <ddtien0909@gmail.com>
2023-08-05 14:33:24 +0700

    fix helper

---
+++ STM32CubeIDE/Application/User/Helper/Board.hpp
@@ -0,0 +1,23 @@
+/*
+ * Board.hpp
+ *
+ *  Created on: Aug 5, 2023
+ *      Author: DONG DUY TIEN
+ */
+
+#ifndef APPLICATION_USER_HELPER_BOARD_HPP_
+#define APPLICATION_USER_HELPER_BOARD_HPP_
+
+class Board
+{
+private:
+	 uint8_t desk[10][10];
+
+public:
+	 Board();
+	 void Init();
+};
+
+
+
+#endif /* APPLICATION_USER_HELPER_BOARD_HPP_ */
